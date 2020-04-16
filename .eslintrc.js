9cdc4cc9ea1c93c55cff391c7a9dd4f75baf8f27
tuanmjnhvnpt <tuanmjnhvnpt@gmail.com>
2020-04-16 10:12:20 +0700

    tm

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   root: true,
 
   parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module"
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
 
   env: {
@@ -13,12 +13,12 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/essential",
-    "@vue/standard"
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
 
   // required to lint *.vue files
-  plugins: ["vue"],
+  plugins: ['vue'],
 
   globals: {
     ga: true, // Google Analytics
@@ -31,35 +31,37 @@ module.exports = {
 
   // add your custom rules here
   rules: {
+    'template-curly-spacing': 'off',
+    indent: 'off',
     // allow async-await
-    "generator-star-spacing": "off",
+    'generator-star-spacing': 'off',
     // allow paren-less arrow functions
-    "no-useless-escape": "off",
-    "arrow-parens": "off",
-    "one-var": "off",
-    semi: "off",
-    "no-unused-vars": "off",
-    "no-redeclare": "off",
-    "handle-callback-err": "off",
-    "import/first": "off",
-    "import/named": "error",
-    "import/namespace": "off",
-    "import/default": "error",
-    "import/export": "error",
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": "off",
-    "prefer-promise-reject-errors": "off",
-    "space-before-function-paren": "off",
-    "comma-dangle": "error",
-    "no-trailing-spaces": "error",
-    "no-mixed-operators": "off",
-    "no-extend-native": "off",
-    "vue/require-prop-type-constructor": "off",
-    "vue/valid-v-bind": "off",
+    'no-useless-escape': 'off',
+    'arrow-parens': 'off',
+    'one-var': 'off',
+    semi: 'off',
+    'no-unused-vars': 'off',
+    'no-redeclare': 'off',
+    'handle-callback-err': 'off',
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'off',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'space-before-function-paren': 'off',
+    'comma-dangle': 'error',
+    'no-trailing-spaces': 'error',
+    'no-mixed-operators': 'off',
+    'no-extend-native': 'off',
+    'vue/require-prop-type-constructor': 'off',
+    'vue/valid-v-bind': 'off',
     // allow console.log during development only
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 };
